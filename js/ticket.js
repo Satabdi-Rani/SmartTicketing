b5e9d561dac3d3b2f8e91dbb93bec62faad00db8
Satabdi <satabdiraniweb@gmail.com>
2024-04-13 14:21:08 +0600

    Details added

--- js/ticket.js
+++ js/ticket.js
@@ -8,16 +8,15 @@ function scrollToSection(){
 const seats = document.getElementsByClassName('seat');
 for (const seat of seats){
     // console.log(seat);
-    
+    let count =0;
     seat.addEventListener("click", function(event){
         const clickedEvent = event.target;
-        
+        count++;
         // Button color change
         event.target.style.backgroundColor = "#1DD100";
         event.target.style.color = "#FFFFFF";
         const ticketSelectedUl = document.getElementById('ticket-SelectedUl');
-
-    
+       
         // if(clickedEvent.classList.contains('seat')){
 
             const listItem = document.createElement('li');
@@ -57,16 +56,45 @@ for (const seat of seats){
         const seatsLeft = parseInt(seatsLeftString);
         // console.log(typeof seatsLeft, seatsLeft);
         seatsLeftBefore.innerText = seatsLeft -1; 
-
-        if(seatNumber>4){
-                alert('You already selected 4 seats');
-            }
+        
+        // if(seatNumber>4){
+        //         alert('You already selected 4 seats');
+        //     }
+        if(count>4){
+            alert("you can only select 4 seats");
+        }
 
         // total price
         const totalPriceAmount = document.getElementById('total-price');
         const totalPriceString = totalPriceAmount.innerText;
         const totalPrice = parseInt(totalPriceString);
-        // console.log(typeof totalPrice, totalPrice);
-       totalPriceAmount.innerText = totalPrice + ticketPrice;
+        totalPriceAmount.innerText = totalPrice + ticketPrice;
+
+        // Grand Price 
+        const grandPriceAmount = document.getElementById('grand-price');
+        const grandPriceString = grandPriceAmount.innerText;
+        const grandPrice = parseInt(grandPriceString);
+        grandPriceAmount.innerText = grandPrice + ticketPrice;
+
+        // Apply coupon
+        const applyButton = document.getElementById('btn-apply');
+        const couponInput = document.getElementById('coupon-input');
+        const coupon = couponInput.value;
+        console.log(coupon);
     })
 }
+
+
+// Next button
+// document.getElementById('btn-next').disabled = true;
+document.getElementById('btn-next').addEventListener('click', function(){
+
+    location.href = "success.html";
+})
+
+
+
+
+
+
+// Continue Button
--- js/utilities.js
+++ js/utilities.js
@@ -3,3 +3,9 @@ function stringToNumber(input){
     const elementStringtoNumber = parseInt(element.innerText);
     return elementStringtoNumber;
 }
+
+
+
+document.getElementById('btn-continue').addEventListener('click', function(){
+    location.href = "index.html";
+})
