a9d83964ddd97fd66d761177ff58fae7f2b5fa34
Satabdi <satabdiraniweb@gmail.com>
2024-04-13 11:31:43 +0600

    Seat before and seats left js added

--- js/ticket.js
+++ js/ticket.js
@@ -7,21 +7,49 @@ function scrollToSection(){
 
 const seats = document.getElementsByClassName('seat');
 for (const seat of seats){
-    console.log(seat);
-    seat.addEventListener("click", function(){
-        // console.log("hi");
-        const ticketNumber = document.getElementById('ticket-selected');
-        const h3One = document.createElement('h3');
-        h3One.innerText = seat.innerText;
-        ticketNumber.appendChild(h3One);
+    // console.log(seat);
+    
+    seat.addEventListener("click", function(event){
+        const clickedEvent = event.target;
+        
 
-        const h3Two = document.createElement('h3');
-        h3Two.innerText = "Economy";
-        ticketNumber.appendChild(h3Two);
+        const ticketSelectedUl = document.getElementById('ticket-SelectedUl');
+        // if(clickedEvent.classList.contains('seat')){
 
-        const h3Three = document.createElement('h3');
-        h3Three.innerText = "500";
-        ticketNumber.appendChild(h3Three);
-        // ticketNumber.innerText = seat.innerText;
+            const listItem = document.createElement('li');
+            listItem.classList.add('flex', 'flex-row', 'justify-between', 'items-center');
+            const h3One = document.createElement('h3');
+            h3One.innerText = seat.innerText;
+            
+
+            const h3Two = document.createElement('h3');
+            h3Two.innerText = "Economy";
+            
+
+            const h3Three = document.createElement('h3');
+            h3Three.innerText = "500"; 
+
+            // ticketSelectedUl.innerHTML = '';
+
+            listItem.appendChild(h3One);
+            listItem.appendChild(h3Two);
+            listItem.appendChild(h3Three);
+            ticketSelectedUl.appendChild(listItem);
+            
+        // }
+        
+        // Changing seat number
+        let seatNumberBefore = document.getElementById('seat-Number');
+        let seatString = seatNumberBefore.innerText;
+        const seatNumber = parseInt(seatString);
+        // console.log(typeof seatNumber, seatNumber);
+        seatNumberBefore.innerText = seatNumber +1;
+        
+        //Seats left
+        const seatsLeftBefore = document.getElementById('seats-left');
+        const seatsLeftString = seatsLeftBefore.innerText;
+        const seatsLeft = parseInt(seatsLeftString);
+        // console.log(typeof seatsLeft, seatsLeft);
+        seatsLeftBefore.innerText = seatsLeft -1; 
     })
 }
---
+++ js/utilities.js
@@ -0,0 +1,5 @@
+function stringToNumber(input){
+    const element = document.getElementById(input);
+    const elementStringtoNumber = parseInt(element.innerText);
+    return elementStringtoNumber;
+}
