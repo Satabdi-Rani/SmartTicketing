126891796658d11a56c0d56a911e4f9ebddfad0a
Satabdi <satabdiraniweb@gmail.com>
2024-04-13 12:33:59 +0600

    total price added

--- js/ticket.js
+++ js/ticket.js
@@ -12,8 +12,12 @@ for (const seat of seats){
     seat.addEventListener("click", function(event){
         const clickedEvent = event.target;
         
-
+        // Button color change
+        event.target.style.backgroundColor = "#1DD100";
+        event.target.style.color = "#FFFFFF";
         const ticketSelectedUl = document.getElementById('ticket-SelectedUl');
+
+    
         // if(clickedEvent.classList.contains('seat')){
 
             const listItem = document.createElement('li');
@@ -25,9 +29,11 @@ for (const seat of seats){
             const h3Two = document.createElement('h3');
             h3Two.innerText = "Economy";
             
-
+            
+            const ticketPrice = stringToNumber('ticket-price');
+            // console.log(typeof ticketPrice, ticketPrice);
             const h3Three = document.createElement('h3');
-            h3Three.innerText = "500"; 
+            h3Three.innerText = ticketPrice; 
 
             // ticketSelectedUl.innerHTML = '';
 
@@ -51,5 +57,16 @@ for (const seat of seats){
         const seatsLeft = parseInt(seatsLeftString);
         // console.log(typeof seatsLeft, seatsLeft);
         seatsLeftBefore.innerText = seatsLeft -1; 
+
+        if(seatNumber>4){
+                alert('You already selected 4 seats');
+            }
+
+        // total price
+        const totalPriceAmount = document.getElementById('total-price');
+        const totalPriceString = totalPriceAmount.innerText;
+        const totalPrice = parseInt(totalPriceString);
+        // console.log(typeof totalPrice, totalPrice);
+       totalPriceAmount.innerText = totalPrice + ticketPrice;
     })
 }
